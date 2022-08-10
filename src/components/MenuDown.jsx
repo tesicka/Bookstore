de1cff6c4d46d115137051c964b75885a1fb50f1
Sara Tesic <sara.tesic5@gmail.com>
2022-08-10 15:54:57 +0200

    Creating react project and menus

---
+++ src/components/MenuDown.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsChevronDoubleRight} from 'react-icons/bs'
+
+function MenuDown() {
+  return (
+    <div className='menuDown'>
+      <div className='menuDownText'>See my favourite books
+      <button className='button_next'>
+        <BsChevronDoubleRight></BsChevronDoubleRight>
+        </button>
+        </div>
+    </div>
+  )
+}
+
+export default MenuDown
---
+++ src/components/MenuUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MenuUp() {
+  return (
+    <div className='menuUp'>
+      <div className='menuUpText'>Sara's bookstore</div>
+    </div>
+  )
+}
+
+export default MenuUp
